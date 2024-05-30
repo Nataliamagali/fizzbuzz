e6736e5a04c450dbb412329994e5fecc8f64bacb
Nataliamagali <natyrodriguez2317@gmail.com>
2024-05-29 23:14:12 -0400

    fizzbuzz

---
+++ Fizzbuzz.js
@@ -0,0 +1,17 @@
+/* Para cada número que sea múltiplo de 3 , imprima  "Fizz"
+Para cada número que sea múltiplo de 5 , imprima  "Buzz"
+Para los números que son múltiplos de 3 y 5, imprima  "FizzBuzz"
+Todos los demás números deberían imprimirse de forma normal*/
+
+for(var i=1; i<101;i++){
+if (i % 15 == 0){
+console.log("FizzBuzz");
+} else if (i % 5 == 0){
+    console.log("Buzz");  
+} else if (i % 15 == 0) {
+console.log("Fizz");
+} else {
+    console.log(i);
+}
+
+}
